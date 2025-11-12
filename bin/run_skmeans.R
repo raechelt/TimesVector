c69d346a3950b8d50d7148baddfa23bd8726a669
raechelt <113154483+raechelt@users.noreply.github.com>
2025-11-13 02:47:58 +0700

    Update run_skmeans.R

--- bin/run_skmeans.R
+++ bin/run_skmeans.R
@@ -33,39 +33,38 @@ gene_exprmat=as.matrix(gene_expr[,2:ncol(gene_expr)])
 # generate initial centers dari data
 init_cent <- as.matrix(read.table("/content/centroid.txt"))
 
-# normalisasi (opsional tapi disarankan untuk spherical)
-init_cent <- init_cent / sqrt(rowSums(init_cent^2))
+# normalisasi (opsional tapi disarankan untuk spherical) -> buat data manual 
+# init_cent <- init_cent / sqrt(rowSums(init_cent^2))
 #
 
 outclust=paste(outdir, "/K", k, ".cluster", sep="")
 outprototype=paste(outdir, "/K", k, ".prototype", sep="")
 cl<-skmeans(gene_exprmat, k, method="genetic", m=1, weights=1)
-# pakai inisialisasi ini untuk skmeans
-cl <- skmeans(gene_exprmat, k,
-              method = "pclust",           # bukan genetic, biar nggak diacak
-              control = list(start = list(init_cent)))
+# pakai inisialisasi ini untuk skmeans -> buat data manual
+# cl <- skmeans(gene_exprmat, k,
+#              method = "pclust",           # bukan genetic, biar nggak diacak
+#              control = list(start = list(init_cent)))
 #
 
-# RAW centroid (belum dinormalisasi)
-raw_centroids <- matrix(NA, nrow=k, ncol=ncol(gene_exprmat))
-for (i in 1:k) {
-  cluster_genes <- gene_exprmat[cl$cluster == i, , drop=FALSE]
-  if (nrow(cluster_genes) > 0) {
-    raw_centroids[i, ] <- colMeans(cluster_genes)
-  }
-}
-colnames(raw_centroids) <- colnames(gene_exprmat)
+# RAW centroid (belum dinormalisasi) -> buat data manual
+# raw_centroids <- matrix(NA, nrow=k, ncol=ncol(gene_exprmat))
+# for (i in 1:k) {
+#  cluster_genes <- gene_exprmat[cl$cluster == i, , drop=FALSE]
+#  if (nrow(cluster_genes) > 0) {
+#    raw_centroids[i, ] <- colMeans(cluster_genes)
+#  }
+#}
+#colnames(raw_centroids) <- colnames(gene_exprmat)
 #
 
-# simpan ke file kalau mau lihat nanti
-write.table(init_cent,
-            file=paste(outdir, "/K", k, ".initial_prototype", sep=""),
-            sep="\t", quote=FALSE)
+# simpan ke file kalau mau lihat nanti -> buat data manual
+# write.table(init_cent,
+#            file=paste(outdir, "/K", k, ".initial_prototype", sep=""),
+#            sep="\t", quote=FALSE)
 #
-# === Simpan ke file ===
-write.table(raw_centroids,
-            file=paste0(outdir, "/K", k, ".raw_centroid"),
-            sep="\t", quote=FALSE, row.names=FALSE)
+#write.table(raw_centroids,
+#            file=paste0(outdir, "/K", k, ".raw_centroid"),
+#            sep="\t", quote=FALSE, row.names=FALSE)
 #
 
 

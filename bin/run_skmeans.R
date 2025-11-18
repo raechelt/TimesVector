912ee6ff297d67161362a3268de719c76b5f1b51
raechelt <113154483+raechelt@users.noreply.github.com>
2025-11-18 15:56:00 +0700

    Update run_skmeans.R

--- bin/run_skmeans.R
+++ bin/run_skmeans.R
@@ -1,92 +1,102 @@
-#TimesVector
-#
-#Created by Inuk Jung on 2016-08-3.
-#Copyright (c) 2016 Inuk Jung. All rights reserved.
-#
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-args<-commandArgs()
+# TimesVector dengan FULL TRACE
+args <- commandArgs()
 library(skmeans)
 
 #reproducible
 set.seed(42)
-#
 
-fname=args[6]
-k=strtoi(args[7])
-outdir=args[8]
-gene_expr<-read.table(fname, header=TRUE)
-rownames(gene_expr)=gene_expr[,1]
-gene_exprmat=as.matrix(gene_expr[,2:ncol(gene_expr)])
+fname <- args[6]
+k <- strtoi(args[7])
+outdir <- args[8]
+gene_expr <- read.table(fname, header=TRUE)
+rownames(gene_expr) <- gene_expr[,1]
+gene_exprmat <- as.matrix(gene_expr[,2:ncol(gene_expr)])
 
-# AUTO-LOAD CUSTOM CENTROID FILE -> manual
-centroid_file <- "centroid.txt"  # Your centroid file name
+cat("=== DATA INPUT ===\n")
+cat("Gene expression data:\n")
+print(gene_exprmat)
+
+# AUTO-LOAD CUSTOM CENTROID FILE
+centroid_file <- "centroid.txt"
 if (file.exists(centroid_file)) {
+  cat("\n=== LOADING CENTROIDS ===\n")
   cat("Using custom centroids from:", centroid_file, "\n")
-
-  #baca centroid file
+  
   init_cent <- as.matrix(read.table(centroid_file, header=FALSE))
-
-  cat("Centroid data loaded:\n")
+  cat("Raw centroids:\n")
   print(init_cent)
   
-  # Normalize centroids for spherical k-means -> data manual
-  init_cent_norm <- init_cent / sqrt(rowSums(init_cent^2))
+  # TRACE: Normalize centroids
+  cat("\n=== NORMALIZING CENTROIDS ===\n")
+  cent_norms <- sqrt(rowSums(init_cent^2))
+  cat("Centroid norms:", cent_norms, "\n")
   
-  # Normalize gene data for spherical k-means -> data manual
-  gene_exprmat_norm <- gene_exprmat / sqrt(rowSums(gene_exprmat^2))
-
-  # pakai inisialisasi ini untuk skmeans -> buat data manual
-  cl <- skmeans(gene_exprmat_norm, k,
-              method = "pclust",           # bukan genetic, biar nggak diacak
-              control = list(start = list(init_cent_norm)))
-
-  } else {
+  init_cent_norm <- init_cent / cent_norms
+  cat("Normalized centroids:\n")
+  print(init_cent_norm)
+  
+  # TRACE: Normalize gene data
+  cat("\n=== NORMALIZING GENE DATA ===\n")
+  gene_norms <- sqrt(rowSums(gene_exprmat^2))
+  cat("Gene norms:\n")
+  print(gene_norms)
+  
+  gene_exprmat_norm <- gene_exprmat / gene_norms
+  cat("Normalized gene data:\n")
+  print(gene_exprmat_norm)
+  
+  # TRACE: Calculate cosine distances to initial centroids
+  cat("\n=== INITIAL COSINE DISTANCES ===\n")
+  for(i in 1:nrow(gene_exprmat_norm)) {
+    dist1 <- 1 - sum(gene_exprmat_norm[i,] * init_cent_norm[1,])
+    dist2 <- 1 - sum(gene_exprmat_norm[i,] * init_cent_norm[2,])
+    cat(rownames(gene_exprmat)[i], 
+        "-> Cent1:", round(dist1, 6), 
+        "-> Cent2:", round(dist2, 6), 
+        "-> Initial Cluster:", ifelse(dist1 < dist2, 1, 2), "\n")
+  }
+  
+  # Run skmeans with custom initialization
+  cat("\n=== RUNNING SKMEANS ===\n")
+  cl <- skmeans(gene_exprmat_norm, k, control = list(start = list(init_cent_norm)))
+  
+} else {
   cat("Custom centroid file not found. Using genetic algorithm.\n")
-  # Original genetic algorithm method
   cl <- skmeans(gene_exprmat, k, method="genetic", m=1, weights=1)
 }
-#
 
-outclust=paste(outdir, "/K", k, ".cluster", sep="")
-outprototype=paste(outdir, "/K", k, ".prototype", sep="")
-#cl<-skmeans(gene_exprmat, k, method="genetic", m=1, weights=1)
+cat("\n=== FINAL RESULTS ===\n")
+cat("Cluster assignments:\n")
+print(cl$cluster)
 
+cat("Final prototypes (centroids):\n")
+print(cl$prototypes)
 
-
-write.table(cl$cluster, file=outclust, sep="\t", quote=FALSE, row.names=FALSE)
-write.table(cl$prototypes, file=outprototype, sep="\t", quote=FALSE, row.names=FALSE)
-
-# RAW centroid (belum dinormalisasi) -> buat data manual
+# Calculate raw centroids from original data
+cat("\n=== RAW CENTROIDS CALCULATION ===\n")
 raw_centroids <- matrix(NA, nrow=k, ncol=ncol(gene_exprmat))
- for (i in 1:k) {
+for (i in 1:k) {
   cluster_genes <- gene_exprmat[cl$cluster == i, , drop=FALSE]
+  cat("Cluster", i, "has", nrow(cluster_genes), "genes:\n")
+  print(rownames(cluster_genes))
   if (nrow(cluster_genes) > 0) {
     raw_centroids[i, ] <- colMeans(cluster_genes)
+    cat("Raw centroid", i, ":", round(raw_centroids[i, ], 6), "\n")
   }
 }
 colnames(raw_centroids) <- colnames(gene_exprmat)
-#
-  
-# simpan ke file kalau mau lihat nanti -> buat data manual
-write.table(init_cent,
-            file=paste(outdir, "/K", k, ".initial_prototype", sep=""),
-            sep="\t", quote=FALSE)
 
-write.table(raw_centroids,
-            file=paste0(outdir, "/K", k, ".raw_centroid"),
+# Output files
+outclust <- paste(outdir, "/K", k, ".cluster", sep="")
+outprototype <- paste(outdir, "/K", k, ".prototype", sep="")
+
+write.table(cl$cluster, file=outclust, sep="\t", quote=FALSE)
+write.table(cl$prototypes, file=outprototype, sep="\t", quote=FALSE)
+write.table(raw_centroids, file=paste0(outdir, "/K", k, ".raw_centroids"),
             sep="\t", quote=FALSE, row.names=FALSE)
-#
-  
+
+cat("\n=== FILES SAVED ===\n")
+cat("Cluster assignments:", outclust, "\n")
+cat("Prototypes:", outprototype, "\n")
+cat("Raw centroids:", paste0(outdir, "/K", k, ".raw_centroids"), "\n")
 cat("Clustering completed successfully!\n")

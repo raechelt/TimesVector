76856bc9a165a94e6bb5a7e6ab7c949764f8081f
raechelt <113154483+raechelt@users.noreply.github.com>
2025-11-20 22:53:48 +0700

    Update run_skmeans.R

--- bin/run_skmeans.R
+++ bin/run_skmeans.R
@@ -16,6 +16,9 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#reproducible
+set.seed(42)
+
 args<-commandArgs()
 library(skmeans)
 fname=args[6]
